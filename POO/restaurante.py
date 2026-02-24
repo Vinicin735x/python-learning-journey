6d8ebee4d9c066cf76b50257a318c4e42c079c8e
Vinicin735x <vini08.mantovani@gmail.com>
2026-02-24 17:04:21 -0300

    Implementação dos métodos str e listar

--- POO/restaurante.py
+++ POO/restaurante.py
@@ -1,14 +1,20 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+         
+    
+restaurante_praca = Restaurante('Restaurante 1', 'Pizzaria')
+restaurante_pizza = Restaurante('Restaurante 2', 'Marmitaria')
 
-restaurante_pizza = Restaurante()
-
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(restaurante_praca.ativo)
+Restaurante.listar_restaurantes()
